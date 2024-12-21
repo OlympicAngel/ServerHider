ec6c1b89d3455f0edbfa6b09fe844e02108fe2cc
Almog <48240399+OlympicAngel@users.noreply.github.com>
2024-12-21 17:07:21 +0200

    added logger & fix the bug when server icon renamaing / loading failed

--- src/main/java/com/olympicangel/serverhider/main.java
+++ src/main/java/com/olympicangel/serverhider/main.java
@@ -2,7 +2,6 @@ package com.olympicangel.serverhider;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.CachedServerIcon;
 
 import java.io.File;
 
@@ -14,17 +13,34 @@ public final class main extends JavaPlugin {
     @Override
     public void onEnable() {
         main.ref = this;
+        //def new icon location
+        iconPath = new File("./server-icon2.png");
+        //register events
         Bukkit.getPluginManager().registerEvents(new Events(),this);
 
-        iconPath = new File("./server-icon.png");
         //if default icon exists rename it to correct way
-        if(iconPath.exists())
-            iconPath.renameTo(this.iconPath);
+        File defaultIconPath = new File("./server-icon.png");
+        if(defaultIconPath.exists()) {
+            Logger("Changing default icon(server-icon.png) => 'server-icon2.png'..");
+            boolean fileNameChanged = defaultIconPath.renameTo(iconPath);
+            if(fileNameChanged)
+                Logger("Icon renaming succeeded! (please do not change manually)");
+            else
+                Logger("Icon renaming FAILED! please rename manually..");
+        }
+        else if(iconPath.exists())
+            Logger("Found hidden server icon (server-icon2.png)");
 
+        Logger("Server is well hidden from scanners / unknown players :)");
     }
 
     @Override
     public void onDisable() {
+        iconPath = null;
         main.ref = null;
+    }
+
+    public static void Logger(String msg){
+        Bukkit.getLogger().info("[ServerHider]: " + msg);
     }
 }
